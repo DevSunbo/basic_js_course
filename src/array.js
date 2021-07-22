e8c1e2159f426335412c05354b5aeb3e3513430f
DevSunbo <30544967+DevSunbo@users.noreply.github.com>
2021-07-22 20:08:42 +0900

    배열 배열사용법 익히기

---
+++ src/array.js
@@ -0,0 +1,8 @@
+const array = [1, 3, 5, 7, 9];
+// console.log(array);
+
+const objects = [{ name: "멍멍이" }, { name: "야옹이" }];
+console.log(objects);
+
+objects.push({ name: "짹짹이" }); // 가장 마지막에 들어간다
+console.log(objects.length);
